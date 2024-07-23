80e94cfce3a91d8b98c99602aab08387422a5e5a
Sristy <126855796+Sristy-78@users.noreply.github.com>
2024-07-24 00:48:06 +0530

    Create array.js

---
+++ week 1/level 1/array.js
@@ -0,0 +1,134 @@
+// Array handbook
+
+// Array:   push(), pop(), shift(), unshift(), splice(), slice(),
+// concat(), forEach(), map(), filter(), reduce(), find(), sort()
+
+// Run each function to see the output, play and learn by doing.
+
+// push()
+function pushExample(arr, element) {
+  console.log("Original Array:", arr);
+
+  arr.push(element);
+  console.log("After push:", arr);
+}
+pushExample([1, 2, 3], 4);
+
+// pop()
+function popExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.pop();
+  console.log("After pop:", arr);
+}
+popExample([1, 2, 3]);
+
+// shift()
+function shiftExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.shift();
+  console.log("After shift:", arr);
+}
+shiftExample([1, 2, 3]);
+
+// unshift()
+function unshiftExample(arr, element) {
+  console.log("Original Array:", arr);
+
+  arr.unshift(element);
+  console.log("After unshift:", arr);
+}
+unshiftExample([1, 2, 3], 0);
+
+// concat()
+function concatExample(arr1, arr2) {
+  console.log("Original Arrays:", arr1, arr2);
+
+  let arr3 = arr1.concat(arr2);
+  console.log("After concat:", arr3);
+}
+concatExample([1, 2, 3], [4, 5, 6]);
+
+// example
+const initialarray=[1,2,3,4,5];
+const secondarray=[6,7,8,9,10];
+console.log(initialarray.concat(secondarray));
+
+// forEach()  similar to foreach loop except the thing that it it
+// is function which require parameters as well
+function forEachExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.forEach(function(item, index) {
+    console.log(item, index);
+  });
+}
+forEachExample([1, 2, 3]);
+
+// example
+function logthing(str){
+  console.log(str);
+}
+initialarray.forEach(logthing)
+logthing(1)
+logthing(2)
+logthing(3)
+
+
+// callback function:-when we pass function inside a function
+function log1(){
+  console.log("hi");
+}
+function log2(){
+  console.log("sristy");
+}
+function logswhatsgiven(fun){
+  fun();
+}
+logswhatsgiven(log2);
+
+// will be covered later in another video
+// map()
+function mapExample(arr) {
+  console.log("Original Array:", arr);
+
+  let newArr = arr.map(function(item) {
+    return item * 2;
+  });
+  console.log("After map:", newArr);
+}
+mapExample([1, 2, 3]);
+
+// filter()
+function filterExample(arr) {
+  console.log("Original Array:", arr);
+
+  let newArr = arr.filter(function(item) {
+    return item > 3;
+  });
+  console.log("After filter:", newArr);
+}
+filterExample([1, 2, 3, 4, 5]);
+
+// find()
+function findExample(arr) {
+  console.log("Original Array:", arr);
+
+  let found = arr.find(function(item) {
+    return item > 3;
+  });
+  console.log("After find:", found);
+}
+findExample([1, 2, 3, 4, 5]);
+
+// sort()
+function sortExample(arr) {
+  console.log("Original Array:", arr);
+
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  console.log("After sort:", arr);
+}
+sortExample([5, 2, 3, 4, 1]);
